d232871b63f6882a5a8f9cd3367ba16dc82dd4c1
florian <florian.even3@gmail.com>
2019-07-19 17:42:04 +0200

    page of the website

---
+++ website-psycho/single.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package Nisarg
+ */
+
+get_header(); ?>
+
+	<div class="container">
+		<div class="row">
+			<div id="primary" class="col-md-12 content-area">
+				<main id="main" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'template-parts/content',get_post_format() ); ?>
+				</main><!-- #main -->
+				<div class="post-navigation">
+					<?php nisarg_post_navigation(); ?>
+				</div>
+				
+				<?php endwhile; // End of the loop. ?>
+			</div><!-- #primary -->
+			
+		</div> <!--.row-->
+	</div><!--.container-->
+	<?php get_footer(); ?>
